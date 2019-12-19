4f17f019b736fe8e3dc0d08be9e1b210a0246469
Nadia27 <jones.nadia.l@gmail.com>
2019-12-18 23:36:48 -0500

    Add Procfile and update ports

--- app.js
+++ app.js
@@ -143,8 +143,12 @@ app.get('/:customListName', (req, res) => {
 });
 
 
+let port = process.env.PORT;
 
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
+app.listen(port, () => {
+  console.log(`Server has started successfully`);
 });
